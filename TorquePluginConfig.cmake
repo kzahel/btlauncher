8db1aacf2b12fce163ec07c24087ab49bd4d8fc4
Patrick Williams <patrick@bittorrent.com>
2012-06-19 12:42:00 -0700

    make the guids unique

--- TorquePluginConfig.cmake
+++ TorquePluginConfig.cmake
@@ -21,12 +21,12 @@ set(IFBComEventSource_DESC "Torque IFBComEventSource Interface")
 set(AXVERSION_NUM "1")
 
 # NOTE: THESE GUIDS *MUST* BE UNIQUE TO YOUR PLUGIN/ACTIVEX CONTROL!  YES, ALL OF THEM!
-set(FBTYPELIB_GUID bb0dcefc-111a-54ea-856b-cbcb610282c7)
-set(IFBControl_GUID e891eefb-5a8b-55d2-80fb-1c965a95069a)
-set(FBControl_GUID 21930925-f5c4-5bfb-b38c-a1b3fd9962aa)
-set(IFBComJavascriptObject_GUID ed803098-f91d-5a53-b869-f62a4cb25144)
-set(FBComJavascriptObject_GUID 5ebd0eec-2267-58e8-ba4b-fa2943462c6f)
-set(IFBComEventSource_GUID edcf337b-5209-51e1-97d7-7c65319c77e6)
+set(FBTYPELIB_GUID bb0dcefc-111a-54ea-856b-cbcb610282c8)
+set(IFBControl_GUID e891eefb-5a8b-55d2-80fb-1c965a95069b)
+set(FBControl_GUID 21930925-f5c4-5bfb-b38c-a1b3fd9962ab)
+set(IFBComJavascriptObject_GUID ed803098-f91d-5a53-b869-f62a4cb25145)
+set(FBComJavascriptObject_GUID 5ebd0eec-2267-58e8-ba4b-fa2943462c60)
+set(IFBComEventSource_GUID edcf337b-5209-51e1-97d7-7c65319c77e7)
 
 # these are the pieces that are relevant to using it from Javascript
 set(ACTIVEX_PROGID "bittorrent.torque")
